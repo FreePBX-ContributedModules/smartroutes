76e6dad47df7fcd0f8db69ce3637689b227725c8
Jay Reeder <jreeder@voicenation.com>
2011-02-17 20:20:47 +0000

    allow '0' seconds for fax delay

--- functions.inc.php
+++ functions.inc.php
@@ -1050,7 +1050,8 @@ function smartroutes_fax_hook_core($viewing_itemid, $target_menuid, $smartroute)
     
     $html.='<tr><td><a href="#" class="info">'._("Fax Detection Time").'<span>'._('How long to wait and try to detect fax. Please note that callers to a '.$dahdi.' channel will hear ringing for this amount of time (i.e. the system wont "answer" the call, it will just play ringing)').'.</span></a>:</td>';
     $html.='<td><select name="faxdetectionwait" tabindex="'.++$tabindex.'">';
-    if(!$fax['detectionwait']){$fax['detectionwait']=4;}//default wait time is 4 second
+    // ejr 2-17-11 allow zero seconds  
+    if($fax['detectionwait'] == ''){$fax['detectionwait']=4;}//default wait time is 4 second
     
     // ejr 2-15-11 allow zero seconds so fax context is created but we don't wait 
     // important for trunks that dialout and connect - fax detected by dahdi after we go out over trunk - no need to wait here
